2c862315ca3e526c483b11410f8af7dd015d8b1b
jeonka1001 <ruddks2009@naver.com>
2021-06-20 17:05:02 +0900

    doc) 조건문 코드 추가

---
+++ go_study/if_else.go
@@ -0,0 +1,28 @@
+
+package main
+// import "fmt"
+
+// func main(){
+
+// 	var age = 10;
+
+// 	if (age >= 18) { 
+// 		fmt.Println("군대 갈 수 있다.");
+// 	} else if age >= 14 {
+// 		fmt.Println("학도병에 지원하세요");
+// 	} else {
+// 		fmt.Println("군대 못가");
+// 	}
+	
+// 	// var name = "hello";
+
+// 	// fmt.Println("if 이전")
+
+// 	// if (name == "hello") {
+// 	// 	fmt.Println("if 안")
+// 	// }
+// 	// fmt.Println("if 밖")
+// }
+
+// // else/else if 의 경우 if 의 닫는 block 과 같은 행에 선언되어야 한다.
+
