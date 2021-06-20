1475a77d45cac10293fe4b1dc148c5d578701775
jeonka1001 <ruddks2009@naver.com>
2021-06-20 17:04:33 +0900

    doc) 변수 선언 방식 추가

--- go_study/hello_world.go
+++ go_study/hello_world.go
@@ -1,10 +1,27 @@
-package main
+package main;
 
-import "fmt"
+/*
+import "fmt";
 
 func main() {
-	fmt.Println("hello world")
+	var testName string = "hello word";
+	var number = 29;
+	num1 := 1;
+	var test int;
+	var boo bool;
+	fmt.Println(testName,number,num1);
+	fmt.Printf("%T %T\n",test, boo);
+	fmt.Println(test, boo );
 }
 
+// 컴파일, 빌드, 실행모듈 환경 설정 오류 
 // go run <file_name>
 // go build <file_name>
+// can't not found moudle : go env -w GO111MODULE=auto
+
+// 다양한 변수 선언법
+// var 변수명 (자료형) : var name string
+// 변수명 := 값 ( 위와 같은 결과 )
+// printf 에서 %T 를 이용해 포멧을 지정하면, 해당 변수의 타입을 출력한다.
+ 
+*/
