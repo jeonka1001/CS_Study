e4ab87162a860d5c5f77d5f909defd90814bcd1e
jeonka1001 <ruddks2009@naver.com>
2021-06-20 17:39:42 +0900

    feat) 주석처리

--- go_study/switch.go
+++ go_study/switch.go
@@ -1,24 +1,37 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-// hello := "hello" 이는 불가능
+// // hello := "hello" 이는 불가능
 
-func main(){
-	var age = 3;
-	switch age{
-	case 1:
-		fmt.Println("초등생1");
-	case 2 :
-		fmt.Println("초등생2");
-	default:
-		fmt.Println("학생 아님");
-	}
+// func main(){
+// 	var age = 0;
+// 	switch age{
+// 	case 1:
+// 		fmt.Println("초등생1");
+// 	case 2 :
+// 		fmt.Println("초등생2");
+// 	default:
+// 		fmt.Println("학생 아님");
+// 	}
 
-	name, num := "jeonka",1;
-	fmt.Printf("%s, %d\n", name, num);
-	name = "jj"
-	fmt.Println(name);
-	name,num2 := "hello",2;
-	fmt.Println(name,num2);
-}
+// 	switch {
+// 		case age > 1 :
+// 			fmt.Println("초딩1")
+// 		case age < 0 :
+// 			fmt.Println("초딩3")
+// 		default :
+// 			fmt.Println("학생 아님")
+// 	}
+
+
+// 	// 다중 선언
+// 	// name, num := "jeonka",1;
+// 	// fmt.Printf("%s, %d\n", name, num);
+// 	// 재 할당 1번째 
+// 	// name = "jj"
+// 	// fmt.Println(name);
+// 	// 재 할당 2번째
+// 	// name,num2 := "hello",2;
+// 	// fmt.Println(name,num2);
+// }
