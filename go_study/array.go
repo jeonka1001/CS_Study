7f9b7c768fc4ffe330f7de0b7b6fc9f11279f75f
jeonka1001 <ruddks2009@naver.com>
2021-06-22 15:57:30 +0900

    go 언어의 기본적인 배열 선언

---
+++ go_study/array.go
@@ -0,0 +1,19 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	var arr [5]int
+// 	arr2 := []int{3, 4, 5}
+// 	arr[0] = 1
+// 	fmt.Println(arr[0])
+// 	fmt.Println(len(arr2))
+// 	aLen := len(arr2)
+// 	sum := 0
+// 	for i := 0; i < aLen; i++ {
+// 		sum += arr2[i]
+// 	}
+// 	fmt.Println(sum)
+// 	arr3 := [][]int{{1, 2}, {2, 3}, {2}}
+// 	fmt.Println(arr3)
+// }
