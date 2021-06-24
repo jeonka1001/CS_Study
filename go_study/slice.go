a4a1cfe46be9c2a94773350565c12deb60db38b6
jeonka1001 <ruddks2009@naver.com>
2021-06-24 12:05:58 +0900

    feat) slice code

--- go_study/slice.go
+++ go_study/slice.go
@@ -1,19 +1,22 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	var x [5]int = [5]int{1, 2, 3, 4, 5}
-	var s []int = x[0:4]
-	var s1 []int = x[1:3]
-	fmt.Println(s)
-	fmt.Println(s1)
-	fmt.Println(len(s))
-	fmt.Println(cap(s))
+// func main() {
+// 	var x [5]int = [5]int{1, 2, 3, 4, 5}
+// 	var s []int
+// 	var s1 []int = x[1:3]
+// 	b := append(x, 7, 8)
+// 	fmt.Println(s)
+// 	fmt.Println(s1)
+// 	fmt.Print("b : ")
+// 	fmt.Println(b)
+// 	fmt.Println(len(s))
+// 	fmt.Println(cap(s))
 
-}
+// }
 
-/*
-length : 배열의 길이
-capacity : 배열의 용량
-*/
+// /*
+// length : 배열의 길이
+// capacity : 배열의 용량
+// */
