d81275327183d61b98bc0d230f2162e06373da96
jeonka1001 <ruddks2009@naver.com>
2021-06-25 15:36:21 +0900

    string 의 format 방법 및 여러가지 포멧 문자

---
+++ go_study/fmt_format.go
@@ -0,0 +1,16 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	num := 1
+// 	fmt.Printf("해당 데이터는 %T 타입이고, 값은 %d 이다.\n", num, num)
+// 	fmt.Printf("format : %t \n", true)
+// 	message := "string test "
+// 	fmt.Print(" % q 는 다음과 같이 표현된다.")
+// 	fmt.Printf("%q\n", message)
+// 	fmt.Print(" % s 는 다음과 같이 표현된다.")
+// 	fmt.Printf("%s\n", message)
+// 	str := fmt.Sprintf("str message : %T 타입이고, 값은 %d 이다.\n", num, num)
+// 	fmt.Println(str)
+// }
